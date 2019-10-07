2af8428bb4bcf9e4b73603bfc0c32a294ebed27c
AL-Khalil-Developers <alkhalildevelopers@gmail.com>
2019-10-07 09:33:52 +0500

    Initial commit

---
+++ app/src/main/java/com/alkhalildevelopers/channel/MainActivity.java
@@ -0,0 +1,26 @@
+package com.alkhalildevelopers.channel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    VideoView videoS;
+    MediaController myMediaController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        videoS = findViewById(R.id.myVideo);
+        videoS.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.intro2);
+        myMediaController = new MediaController(this);
+        myMediaController.setAnchorView(videoS);
+        videoS.setMediaController(myMediaController);
+        videoS.start();
+
+
+    }
+}
